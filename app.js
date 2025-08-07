b2e8a7dd319e2e088c01cbb178dd96abf86841e7
daimyo-rb <daimyo@roshibros.com>
2025-08-06 22:52:36 -0500

    Fix formatting

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`mini msg board app listening on ${PORT}`);
