edeee1dc0b23efd008f905efe6c7f081f983509a
daimyo-rb <daimyo@roshibros.com>
2025-08-06 22:51:57 -0500

    Add show message details (added id to message object)

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date()
@@ -18,11 +20,16 @@ const indexRouter = Router();
 indexRouter.get('/', (req, res, next) => {
   res.render('index', { title: "Mini Messageboard", messages: messages });
 });
+indexRouter.get('/messages/:id', (req, res, next) => {
+  const msg = messages.find((msg) => msg.id == req.params.id);
+  res.render('message', { message: msg });
+});
 indexRouter.get('/new', (req, res, next) => {
   res.render('form');
 });
 indexRouter.post('/new', (req, res, next) => {
-  messages.push({ text: req.body.message, user: req.body.author, added: new Date() });
+  const nextId = messages[messages.length - 1].id + 1;
+  messages.push({ id: nextId, text: req.body.message, user: req.body.author, added: new Date() });
   res.redirect('/');
 });
 
